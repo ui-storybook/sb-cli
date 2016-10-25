bccaf0e00757a3052a4101293b328281a4dc7a08
freeman14 <dubovetzky@me.com>
2016-10-25 22:17:51 +0300

    Minor fix

--- generators/ng/template/welcome/welcome.js
+++ generators/ng/template/welcome/welcome.js
@@ -1,11 +1,11 @@
 import './welcome.css';
 
-let module = angualar.module('welcome', []);
+let module = angular.module('welcome', []);
 
 module.component('sbWelcome', {
   template: `<div class="welcome__card">
                 Hello there and welcome to SB!<br/>
-                This demo component we build for you with React and SB.<br/>
+                This demo component we build for you with Angular and SB.<br/>
                 You can change it <span>'sb/stories/welcome.js'</span> file.<br/>
                 For more information visit <a href="https://github.com/ui-storybook/sb" target="_blank">this page</a><br/>
                 And happy codding! 
